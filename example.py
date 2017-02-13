249461d5fbf80bd21af3e84cb2d7c3f38275f3c9
Thomas Boudreaux <thomas@boudreauxmail.com>
2017-02-13 16:40:21 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,9 +2,7 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 import pandas as pd
 import numpy as np
-import jd_corr
 import os
-from jdcal import gcal2jd, jd2gcal
 import getBJD
 
 # Given some root folder (The exampleFiles.zip file -- to use unzip that in local directory)
@@ -29,19 +27,6 @@ c = SkyCoord('12h49m37.690000s', '-63d32m8.9900000s', frame='icrs')
 params = [JD, "{}:{}:{}".format(str(int((c.ra.hms)[0])), str(int((c.ra.hms)[1])), str((c.ra.hms)[2])),
         "{}:{}:{}".format(str(int((c.dec.dms)[0])), str(abs(int((c.dec.dms)[1]))), str(abs((c.dec.dms)[2]))),
         '-30.1716', '-70.8009', '2172']
-extra = []
-for i in UTC:
-    extra.append(float(i[0]) + float(i[1])/60.0 + float(i[2])/3600.0)
-cJD = []
-for i, j in zip(Date, extra):
-    # print i[0], i[1], i[2]
-    # cJD.append(sum(gcal2jd(i[0], i[1], i[2])))
-    cJD.append(jd_corr.date_to_jd(float(i[0]), float(i[1]), float(i[2])+j/24.0))
-# cJD = [x+(y/86400) for x, y in zip(cJD, extra)]
-
-params2 = [cJD, "{}:{}:{}".format(str(int((c.ra.hms)[0])), str(int((c.ra.hms)[1])), str((c.ra.hms)[2])),
-        "{}:{}:{}".format(str(int((c.dec.dms)[0])), str(abs(int((c.dec.dms)[1]))), str(abs((c.dec.dms)[2]))),
-        '-30.1716', '-70.8009', '2172']
 
 #print params
 print params[0], params[1], params[2], params[3], params[4], params[5]
@@ -53,11 +38,5 @@ kwargs = {
     "iLON": params[4],
     "iALT": params[5]
 }
-# bjd = CustomBaryCorr.GetBJD(params[0], params[1], params[2], params[3], params[4], params[5])
 bjd = CustomBaryCorr.GetBJD(**kwargs)
-pbjd = CustomBaryCorr.GetBJD(*params2)
-#print bjd
-# print cJD
-# rBJD = [y - x for x, y in zip(rBJD, JD)]
-# bjd = [y - x for x, y in zip(bjd, JD)]
-# err = [(abs((x-y)/y))*100 for x, y in zip(bjd, rBJD)]
+print bjd
