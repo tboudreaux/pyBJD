5017b1bb69f4e2891359e5bf8c75eae94a574dfb
Thomas Boudreaux <thomas@boudreauxmail.com>
2017-05-29 11:48:04 -0400

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from os import path
+
+__version__ = 1.0
+
+setup(name='pyBJD',
+	  version=1.0,
+	  description='A Very Basic JD to BJD conversion API',
+	  url='https://github.com/tboudreaux/pyBJD',
+	  author='Thomas Boudreaux',
+	  author_email='thomas@boudreauxmail.com',
+	  license='MIT',
+	  classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+      install_requires=['astropy>=1.3.2',
+      					'numpy>=1.12.0',
+      					'selinium>=3.4.2'],
+      packages=['pyBJD'],
+      zip_safe=False
+	)
